862597a4fc42077074d72023632d77cd58e9bffb
Taylor Roebuck <taylornroebuck@gmail.com>
2020-02-23 20:15:03 -0700

    hooked up welcome page and characters page with react router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList";
 import CharacterCard from "./components/CharacterCard.js";
+import { Route, Switch } from "react-router-dom";
+import WelcomePage from "./components/WelcomePage"
 
 export default function App() {
   return (
     <main>
       <Header />
-      <CharacterList />
+      <Route path="/" exact component={WelcomePage} />
+      <Route path="/characters" exact component={CharacterList} />
+      {/* <Header />
+      <Switch>
+        <Route exact path="/" exact component={WelcomePage} />
+        <Route exact path="/characters" exact component={CharacterList} />
+      </Switch> */}
     </main>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <nav>
+        <NavLink to="/">Home </NavLink>
+        <NavLink to="/characters">Characters </NavLink>
+      </nav>
     </header>
   );
 }
